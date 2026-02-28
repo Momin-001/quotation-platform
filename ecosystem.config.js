c350301d634682afd10d77bcc80882cd75a5022d
Momin-001 <kingmomin789@gmail.com>
2026-02-28 10:05:35 +0500

    fixes

---
+++ ecosystem.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    apps: [
+      {
+        name: "quoationplatform",
+        script: "./server.js",
+        env: {
+          NODE_ENV: "production",
+          PORT: 3000
+        }
+      }
+    ]
+  }
